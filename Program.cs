57a01cb625c180561a06ef9590d3355f62082f40
Harish Raju <harisharaju1@gmail.com>
2024-05-28 22:32:29 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using SampleClassLibrary;
+using System;   
+using System.Collections.Generic;
+
+namespace SampleConsoleApp
+{    
+    public class SampleConsoleApp
+    {
+        public static void Main()
+        {
+            List<int> arr = new List<int> { -7, 0, 1, 2, 3, 5, 5 };
+            var index = BinarySearch.BinarySearchMethod(arr, 5);
+            Console.WriteLine(index.ToString());
+        }
+    }
+}
